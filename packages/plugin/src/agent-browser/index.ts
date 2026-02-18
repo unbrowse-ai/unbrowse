93041977cb3f565f3a2a0db964c7305abc743573
lewistham9x <lewistham9x@gmail.com>
2026-02-18 09:00:48 +0800

    Extract plugin into standalone pkg

---
+++ packages/plugin/src/agent-browser/index.ts
@@ -0,0 +1,4 @@
+export { runAgentBrowser, runAgentBrowserJson } from "./runner.js";
+export { snapshotInteractive, type InteractiveElement } from "./snapshot.js";
+export { captureHarFromAgentBrowser } from "./har.js";
+
